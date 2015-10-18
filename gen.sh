483a0fb497b70df11956cf23f9c85fed9ba0c48a
Harsh Vakharia <harshjv@gmail.com>
2015-10-18 10:44:17 +0530

    Adapt new asset naming pattern for Brew installation

--- gen.sh
+++ gen.sh
@@ -4,8 +4,11 @@ VERSION=`curl -s -L https://api.github.com/repos/yudai/gotty/releases/latest | j
 
 SHASUMS=`curl -s -L "https://github.com/yudai/gotty/releases/download/${VERSION}/SHASUMS"`
 
-SHA1_DARWIN=`echo "${SHASUMS}" | grep darwin_amd64.tar.gz | cut -f 1 -d ' '`
-SHA1_LINUX=`echo "${SHASUMS}" | grep linux_amd64.tar.gz | cut -f 1 -d ' '`
+DARWIN_ASSET_NAME="gotty_darwin_amd64.tar.gz"
+LINUX_ASSET_NAME="gotty_linux_amd64.tar.gz"
+
+SHA1_DARWIN=`echo "${SHASUMS}" | grep ${DARWIN_ASSET_NAME} | cut -f 1 -d ' '`
+SHA1_LINUX=`echo "${SHASUMS}" | grep ${LINUX_ASSET_NAME} | cut -f 1 -d ' '`
 
 echo "VERSION: ${VERSION}"
 echo "SHA1 DARWIN: ${SHA1_DARWIN}"
@@ -18,11 +21,11 @@ class Gotty < Formula
   homepage "https://github.com/yudai/gotty"
   version '$VERSION'
 
-  url "https://github.com/yudai/gotty/releases/download/$VERSION/darwin_amd64.tar.gz"
+  url "https://github.com/yudai/gotty/releases/download/$VERSION/${DARWIN_ASSET_NAME}"
   sha1 "$SHA1_DARWIN"
 
   if OS.linux? && Hardware.is_64_bit?
-      url "https://github.com/yudai/gotty/releases/download/$VERSION/linux_amd64.tar.gz"
+      url "https://github.com/yudai/gotty/releases/download/$VERSION/${LINUX_ASSET_NAME}"
       sha1 "$SHA1_LINUX"
   end
 
